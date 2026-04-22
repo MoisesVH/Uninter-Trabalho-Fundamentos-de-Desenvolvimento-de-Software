c0cb2e5872375d6e4c224552ba7261c2125caf0c
Moisés Viana Honorato <moisesbr2005@gmail.com>
2026-04-22 12:42:20 -0300

    Delete Trabalho-Fundamentos-de-Desenvolvimento-de-Software directory

--- Trabalho-Fundamentos-de-Desenvolvimento-de-Software/script.js
+++
@@ -1,7 +0,0 @@
-let botao = document.querySelector("#botao");
-let msg = document.querySelector("#msg")
-botao.addEventListener("click", aparecerMensagem);
-function aparecerMensagem() {
-    msg.innerHTML="Sempre parecerá impossivel até que seja feito!"
-    msg.classList.add("form")
-}
